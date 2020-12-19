0f13ab3e64d636aa3ee3ef04144ea35586aba14c
ARSENYGAVRILENKO <76223384+ARSENYGAVRILENKO@users.noreply.github.com>
2020-12-19 14:13:25 +0300

    Add files via upload

---
+++ client.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+#define PAGE_SIZE 4096
+
+int main(int argc, char* argv[])
+{
+	int rd = -1;
+	char fifo_client[10] = "";
+printf("%d", getpid());
+	sprintf(fifo_client, "fifo.%d", getpid());
+	if(mkfifo(fifo_client, 0777) == -1)
+	{
+		if(errno != EEXIST)
+		{
+			printf("Could'n create fifo");
+			perror("mkfifo");
+			return 1;
+		}
+	}
+	int wr = -1;
+	int pid = getpid();
+	int fd_serv = open("fifo_server", O_WRONLY);
+	int length = strlen(argv[1]);
+printf("pid = %d, length =  %d", pid, length);
+	if(wr = write(fd_serv, &length, sizeof(int))<0)
+	{
+		printf("Unable to write data");
+	}
+	if(write(fd_serv, argv[1], sizeof(char) * length) < 0)
+	{
+		printf("Unable to write data");
+	}
+	if(write(fd_serv, &pid, sizeof(int)) < 0)
+	{
+		printf("Unable to write data");
+	}
+	int fd_cl = open(fifo_client, O_RDONLY);
+	
+	char buf[PAGE_SIZE] = "";
+
+	while(1)
+	{
+		if(rd = read(fd_cl, buf, PAGE_SIZE) < 0)
+			perror("Unable to read the data from fifo");
+		if(rd == 0)
+			break;
+		if(write(1, buf, rd) < 0)
+		{
+			perror("Unable to write file data to stdout");
+			break;
+		}
+	}
+	close(fd_serv);
+	close(fd_cl);
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+#define PAGE_SIZE 4096
+
+int main(int argc, char* argv[])
+{
+printf("check");
+	if(mkfifo("fifo_server", 0777) == -1)
+	{
+		if(errno != EEXIST)
+		{
+			perror("Could'n create fifo");
+			return 1;
+		}
+	}
+	while(1)
+	{
+		char buf[PAGE_SIZE] = "";
+		int rd = -1;
+		int fd_serv = open("fifo_server", O_RDONLY);
+		int length = 0;
+		if(read(fd_serv, &length, sizeof(int)) < 0)
+		{
+			printf("Make sure you input correct parametrs");
+			continue;
+		}
+		char* file_name = malloc(sizeof(char)*length);
+		if(read(fd_serv, file_name, sizeof(char) * length) < 0)
+		{
+			printf("Make sure you input correct parameters");
+			continue;
+		}
+		int pid;
+		if(read(fd_serv, &pid, sizeof(int)) < 0)
+		{
+			printf("Make sure you input correct parameters");
+			continue;
+		}
+		char fifo_cl[10] = "";
+		sprintf(fifo_cl, "fifo.%d", pid);
+		int file_fd = -1;
+		if(file_fd = open(file_name, O_RDONLY) < 0)
+		{
+			perror("Unable to open requested file");
+			continue;
+		}
+		int fd_cl = open(fifo_cl, O_WRONLY);
+		while(1)
+		{
+			if(rd = read(file_fd, buf, PAGE_SIZE) < 0)
+				perror("Unable to read the file");
+			if(rd == 0)
+				break;
+			if(write(fd_cl, buf, rd) < 0)
+			{	
+				perror("Unable to write file data to fifo");
+				break;
+			}
+		}
+		free(file_name);
+		close(fd_cl);
+		close(fd_serv);
+	}
+}
